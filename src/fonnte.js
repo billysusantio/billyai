b38a7bac44097a813bcb683dfac2eaa65c3dbab8
billysusantio <billy.susantio@gmail.com>
2026-06-14 00:21:41 +0700

    Update fonnte.js

--- src/fonnte.js
+++ src/fonnte.js
@@ -4,18 +4,22 @@ const FONNTE_SEND_URL = "https://api.fonnte.com/send";
 
 /**
  * Send a WhatsApp message through Fonnte.
- * @param {string} target - recipient number in international format without '+', e.g. 6281234567890
+ * @param {string} target - recipient number or group ID
  * @param {string} message - text to send
  * @returns {Promise<object>} Fonnte API response data
  */
 export async function sendMessage(target, message) {
-  const token = process.env.FONNTE_TOKEN;
-  if (!token) throw new Error("FONNTE_TOKEN is not set");
+    const token = process.env.FONNTE_TOKEN;
+    if (!token) throw new Error("FONNTE_TOKEN is not set");
+
+  console.log(`[fonnte] sending to ${target}`);
 
   const res = await axios.post(
-    FONNTE_SEND_URL,
+        FONNTE_SEND_URL,
     { target, message },
     { headers: { Authorization: token } }
-  );
-  return res.data;
+      );
+
+  console.log(`[fonnte] response:`, JSON.stringify(res.data));
+    return res.data;
 }
